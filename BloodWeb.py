147048d69001741a56367514d4ae508c8c0cfc21
MicaJustman <micarunes@gmail.com>
2022-12-11 01:48:38 -0700

    id and display formatted and done

--- BloodWeb.py
+++ BloodWeb.py
@@ -1,4 +1,6 @@
 import os
+from time import sleep
+
 import pygame
 from PIL.Image import frombuffer, fromarray
 from numpy import asarray
@@ -13,7 +15,8 @@ import torch
 import torchvision
 
 DBDhwnd = None
-mode = 0  # mode 0 for predicting nodes, mode 1 for highlighting node locations on screen, mode 2 for grabbing nodes for pytorch model directory
+mode = 0  # mode 0 for main run, mode 1 for highlighting node locations on screen, mode 2 for grabbing nodes for pytorch model directory
+display = 1 # display 1 for show predictions
 mouse = Controller()
 halt = False
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -41,12 +44,12 @@ AdjecentDict = {
 }
 
 # monitor coords of each node spot
-webIndex = [(610, 469), (705, 523), (706, 634), (611, 690), (516, 634), (516, 523), (555, 367), (666, 368), (771, 424),
-            (828, 524),
-            (828, 634), (771, 733), (667, 789), (555, 790), (450, 734), (394, 633), (393, 523), (450, 424), (449, 304),
-            (611, 260),
-            (773, 304), (892, 421), (935, 578), (891, 736), (774, 853), (611, 896), (449, 854), (331, 734), (286, 578),
-            (331, 422)]
+webIndex = [(610, 469), (705, 523), (706, 634), (611, 690), (516, 634),
+            (516, 523), (555, 367), (666, 368), (771, 424), (828, 524),
+            (828, 634), (771, 733), (667, 789), (555, 790), (450, 734),
+            (394, 633), (393, 523), (450, 424), (449, 304), (611, 260),
+            (773, 304), (892, 421), (935, 578), (891, 736), (774, 853),
+            (611, 896), (449, 854), (331, 734), (286, 578), (331, 422)]
 
 
 # stops the code
@@ -106,10 +109,8 @@ width = size[2]
 height = size[3]
 
 if mode == 0:
-    counter = 0
     images = []
     nodes = []
-    lines = []
 
     # creates the listner to stop the code
     listener = keyboard.Listener(on_press=on_press)
@@ -121,48 +122,50 @@ if mode == 0:
     NodeModel.load_state_dict(torch.load("NodeModel.pth"))
     NodeModel.eval()
 
-    # grabs the screen
-    img = grabImage(width, height, 0, 0, DBDhwnd)
-    fromarray(img).save("test.png")
+    while True:
+        # grabs the screen
+        img = grabImage(width, height, 0, 0, DBDhwnd)
+        fromarray(img).save("test.png")
 
-    # predicts each node location and outputs
-    for x in range(30):
-        temp = fromarray(img[webIndex[x][1] - 40:webIndex[x][1] + 40, webIndex[x][0] - 40:webIndex[x][0] + 40])
-        output = NodeModel(transform(temp).unsqueeze(0))
-        output_idx = torch.argmax(output)
-        nodes.append(output_idx)
-
-    # pygame init display
-    pygame.init()
-    screen = pygame.display.set_mode((width, height), pygame.NOFRAME)
-    done = False
-    fuchsia = (255, 0, 128)  # Transparency color
-    blue = (0, 0, 255)
-    green = (0, 255, 0)
-
-    # Create layered window
-    hwnd = pygame.display.get_wm_info()["window"]
-    SetWindowLong(hwnd, GWL_EXSTYLE, GetWindowLong(hwnd, GWL_EXSTYLE) | WS_EX_LAYERED)
-
-    # Set window transparency color
-    SetLayeredWindowAttributes(hwnd, RGB(*fuchsia), 0, LWA_COLORKEY)
-    SetWindowPos(hwnd, HWND_TOP, 0, 0, width, height, SWP_NOSIZE)
-
-    # draws the nodes in color
-    while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-
-        screen.fill(fuchsia)  # Transparent background
-
+        # predicts each node location and outputs
         for x in range(30):
-            if nodes[x] == 0:
-                pygame.draw.arc(screen, blue, pygame.Rect(webIndex[x][0] - 40, webIndex[x][1] - 40, 80, 80), 0, 360, 5)
-            else:
-                pygame.draw.arc(screen, green, pygame.Rect(webIndex[x][0] - 40, webIndex[x][1] - 40, 80, 80), 0, 360, 5)
+            temp = fromarray(img[webIndex[x][1] - 40:webIndex[x][1] + 40, webIndex[x][0] - 40:webIndex[x][0] + 40])
+            output = NodeModel(transform(temp).unsqueeze(0))
+            output_idx = torch.argmax(output)
+            nodes.append(output_idx)
 
-        pygame.display.update()
+        #displays the prediction
+        if display:
+            # pygame init display
+            pygame.init()
+            screen = pygame.display.set_mode((width, height), pygame.NOFRAME)
+            done = False
+            fuchsia = (255, 0, 128)  # Transparency color
+            blue = (0, 0, 255)
+            green = (0, 255, 0)
+            red = (255, 0, 0)
+
+            # Create layered window
+            hwnd = pygame.display.get_wm_info()["window"]
+            SetWindowLong(hwnd, GWL_EXSTYLE, GetWindowLong(hwnd, GWL_EXSTYLE) | WS_EX_LAYERED)
+
+            # Set window transparency color
+            SetLayeredWindowAttributes(hwnd, RGB(*fuchsia), 0, LWA_COLORKEY)
+            SetWindowPos(hwnd, HWND_TOP, 0, 0, width, height, SWP_NOSIZE)
+
+            screen.fill(fuchsia)  # Transparent background
+
+            for x in range(30):
+                if nodes[x] == 0:
+                    pygame.draw.arc(screen, blue, pygame.Rect(webIndex[x][0] - 40, webIndex[x][1] - 40, 80, 80), 0, 360, 5)
+                elif nodes[x] == 1:
+                    pygame.draw.arc(screen, green, pygame.Rect(webIndex[x][0] - 40, webIndex[x][1] - 40, 80, 80), 0, 360, 5)
+                else:
+                    pygame.draw.arc(screen, red, pygame.Rect(webIndex[x][0] - 40, webIndex[x][1] - 40, 80, 80), 0, 360, 5)
+
+            pygame.display.update()
+            sleep(5)
+            exit(0)
 
 elif mode == 1:
     # uncomment this line and clear webIndex to record new indexes. Hit any key to store the location of the mouse
