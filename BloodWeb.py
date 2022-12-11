db029ec3ad9771313169dcc2be359c6b359ba7a4
MicaJustman <micarunes@gmail.com>
2022-12-10 23:02:34 -0700

    weird delete

--- BloodWeb.py
+++ BloodWeb.py
@@ -13,11 +13,11 @@ import torch
 import torchvision
 
 DBDhwnd = None
-mode = 0 #mode 0 for predicting nodes, mode 1 for highlighting node locations on screen, mode 2 for grabbing nodes for pytorch model directory
+mode = 0  # mode 0 for predicting nodes, mode 1 for highlighting node locations on screen, mode 2 for grabbing nodes for pytorch model directory
 mouse = Controller()
 halt = False
 
-#node adj list for ordering nodes
+# node adj list for ordering nodes
 AdjecentDict = {
     1: [18, 7, 8, 9],
     2: [8, 9, 10, 11],
@@ -39,7 +39,7 @@ AdjecentDict = {
     18: [30, 19]
 }
 
-#monitor coords of each node spot
+# monitor coords of each node spot
 webIndex = [(610, 469), (705, 523), (706, 634), (611, 690), (516, 634), (516, 523), (555, 367), (666, 368), (771, 424),
             (828, 524),
             (828, 634), (771, 733), (667, 789), (555, 790), (450, 734), (394, 633), (393, 523), (450, 424), (449, 304),
@@ -47,19 +47,22 @@ webIndex = [(610, 469), (705, 523), (706, 634), (611, 690), (516, 634), (516, 52
             (773, 304), (892, 421), (935, 578), (891, 736), (774, 853), (611, 896), (449, 854), (331, 734), (286, 578),
             (331, 422)]
 
-#stops the code
+
+# stops the code
 def on_press(key):
     global halt
     halt = True
 
-#finds the dbd window handle
+
+# finds the dbd window handle
 def winEnumHandler(hwnd, ctx):
     if IsWindowVisible(hwnd):
         if str(GetWindowText(hwnd)) == 'DeadByDaylight  ':
             global DBDhwnd
             DBDhwnd = hwnd
 
-#grabs the screen
+
+# grabs the screen
 def grabImage(width, height, offset_width, offset_height, DBDhwnd):
     wDC = GetWindowDC(DBDhwnd)
     dcObj = CreateDCFromHandle(wDC)
@@ -81,14 +84,16 @@ def grabImage(width, height, offset_width, offset_height, DBDhwnd):
     DeleteObject(dataBitMap.GetHandle())
     return img
 
-#records mouse coords on press and prints at 30
+
+# records mouse coords on press and prints at 30
 def recordWeb(key):
     global webIndex
     webIndex.append((mouse.position[0], mouse.position[1]))
     if len(webIndex) == 30:
         print(webIndex)
 
-#transform function for pytorch model
+
+# transform function for pytorch model
 transform = torchvision.transforms.Compose([
     torchvision.transforms.ToTensor(),
     torchvision.transforms.Normalize([.5], [.5])
@@ -105,29 +110,24 @@ if mode == 0:
     images = []
     nodes = []
 
-    #creates the listner to stop the code
+    # creates the listner to stop the code
     listener = keyboard.Listener(on_press=on_press)
     print("Press any key to stop")
     listener.start()
 
-    #loads the model
-    model = torchvision.models.resnet18().to(device)
-    model.load_state_dict(torch.load("Model.pth"))
-    model.eval()
+    # loads the model
+    NodeModel = torchvision.models.resnet18().to(device)
+    NodeModel.load_state_dict(torch.load("NodeModel.pth"))
+    NodeModel.eval()
 
-    #grabs the screen
+    # grabs the screen
     img = grabImage(width, height, 0, 0, DBDhwnd)
     fromarray(img).save("test.png")
 
-    #clears web directory
-    for f in os.listdir('web'):
-        os.remove(os.path.join('web', f))
-
-    #predicts each node location and outputs
+    # predicts each node location and outputs
     for x in range(30):
         temp = fromarray(img[webIndex[x][1] - 40:webIndex[x][1] + 40, webIndex[x][0] - 40:webIndex[x][0] + 40])
-        temp.save("web/" + str(x) + ".png")
-        output = model(transform(temp).unsqueeze(0))
+        output = NodeModel(transform(temp).unsqueeze(0))
         output_idx = torch.argmax(output)
         nodes.append(output_idx)
 
@@ -157,9 +157,9 @@ if mode == 0:
 
         for x in range(30):
             if nodes[x] == 0:
-                pygame.draw.arc(screen, blue, pygame.Rect(webIndex[x][0] - 40, webIndex[x][1] - 40, 80, 80) , 0, 360, 5)
+                pygame.draw.arc(screen, blue, pygame.Rect(webIndex[x][0] - 40, webIndex[x][1] - 40, 80, 80), 0, 360, 5)
             else:
-                pygame.draw.arc(screen, green, pygame.Rect(webIndex[x][0] - 40, webIndex[x][1] - 40, 80, 80) , 0, 360, 5)
+                pygame.draw.arc(screen, green, pygame.Rect(webIndex[x][0] - 40, webIndex[x][1] - 40, 80, 80), 0, 360, 5)
         pygame.display.update()
 
 elif mode == 1:
@@ -167,7 +167,7 @@ elif mode == 1:
     '''with keyboard.Listener(on_press=recordweb) as listener:
             listener.join()'''
 
-    #pygame init display
+    # pygame init display
     pygame.init()
     screen = pygame.display.set_mode((width, height), pygame.NOFRAME)
     done = False
@@ -182,7 +182,7 @@ elif mode == 1:
     SetLayeredWindowAttributes(hwnd, RGB(*fuchsia), 0, LWA_COLORKEY)
     SetWindowPos(hwnd, HWND_TOP, 0, 0, width, height, SWP_NOSIZE)
 
-    #draws the nodes in color
+    # draws the nodes in color
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -200,12 +200,17 @@ elif mode == 2:
     time_now = datetime.now()
     current_time = time_now.strftime("%H%M%S")
 
-    #grabs the screen
+    # grabs the screen
     img = grabImage(width, height, 0, 0, DBDhwnd)
     fromarray(img).save("test.png")
 
-    #saves the node pics for addition to the machine learning folders
+    # saves the node pics for addition to the machine learning folders
     for x in webIndex:
         temp = fromarray(img[x[1] - 40:x[1] + 40, x[0] - 40:x[0] + 40])
-        temp.save('web/' + str(current_time) +str(counter) + ".png")
+        temp.save('web/' + str(current_time) + str(counter) + ".png")
         counter += 1
+
+elif mode == 3:
+    # clears web directory
+    for f in os.listdir('web'):
+        os.remove(os.path.join('web', f))
